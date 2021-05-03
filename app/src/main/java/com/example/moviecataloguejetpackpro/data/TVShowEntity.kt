9f499553105225944b9ef88eeb7b3ad5a93a1b75
Dalo Barahama <das.barahama@gmail.com>
2021-05-03 15:09:38 +0800

    finished add all dummy data

---
+++ app/src/main/java/com/example/moviecataloguejetpackpro/data/TVShowEntity.kt
@@ -0,0 +1,10 @@
+package com.example.moviecataloguejetpackpro.data
+
+data class TVShowEntity(
+    var tvShowId: String,
+    var title: String,
+    var overview: String,
+    var tags: String,
+    var score: String,
+    var releaseDate: String,
+)
