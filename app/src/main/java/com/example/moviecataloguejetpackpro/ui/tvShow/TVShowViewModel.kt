03ed334046cb7a3f923776eb2ca739e6ea3b09dd
Dalo Barahama <das.barahama@gmail.com>
2021-05-29 12:32:36 +0700

    update some code

--- app/src/main/java/com/example/moviecataloguejetpackpro/ui/tvShow/TVShowViewModel.kt
+++ app/src/main/java/com/example/moviecataloguejetpackpro/ui/tvShow/TVShowViewModel.kt
@@ -1,10 +1,21 @@
 package com.example.moviecataloguejetpackpro.ui.tvShow
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.moviecataloguejetpackpro.data.Entity
 import com.example.moviecataloguejetpackpro.utils.DataDummy
 
 class TVShowViewModel : ViewModel() {
+
+    var tvShows: MutableLiveData<List<Entity>>? = null
+        get() {
+            if (field == null) {
+                field = MutableLiveData<List<Entity>>()
+                getTVShow()
+            }
+            return field
+        }
+        private set
 
     fun getTVShow(): List<Entity> = DataDummy.generateDummyTVShows()
 
