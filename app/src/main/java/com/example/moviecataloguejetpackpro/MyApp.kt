365d29258eb408782546875096a79c3a0eb19511
Dalo Barahama <das.barahama@gmail.com>
2024-07-15 11:54:43 +0700

    dev: clean up code

--- app/src/main/java/com/example/moviecataloguejetpackpro/MyApp.kt
+++ app/src/main/java/com/example/moviecataloguejetpackpro/MyApp.kt
@@ -1,12 +1,13 @@
 package com.example.moviecataloguejetpackpro
 
 import android.app.Application
+import com.example.moviecataloguejetpackpro.common.di.app.AppComponent
 import com.example.moviecataloguejetpackpro.common.di.app.AppModule
 import com.example.moviecataloguejetpackpro.common.di.app.DaggerAppComponent
 
 class MyApp : Application() {
 
-    val appComponent by lazy {
+    val appComponent: AppComponent by lazy {
         DaggerAppComponent.builder()
             .appModule(AppModule(this))
             .build()
